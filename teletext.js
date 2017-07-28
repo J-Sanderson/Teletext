247180ddf71cf076fc32c596e43f3fcb3480374d
J Sanderson <leopardcorgi@gmail.com>
2017-07-28 09:47:09 +0100

    fix issues with single digit hours/minutes

--- teletext.js
+++ teletext.js
@@ -1,20 +1,47 @@
-var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+var months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 function tick() {
+  var currTime = new Date();
+  var currMin;
+  var currHour;
 
-var currTime = new Date();
+  //leading zeroes
+  if (String(currTime.getMinutes()).length == 1) {
+    currMin = "0" + String(currTime.getMinutes());
+  } else {
+    currMin = String(currTime.getMinutes());
+  }
 
-document.getElementById("header").innerHTML =
-  days[currTime.getDay()] +
-  " " +
-  months[currTime.getMonth()] +
-  " " +
-  currTime.getDate() +
-  " " +
-  currTime.getHours() +
-  ":" +
-  currTime.getMinutes();
+  if (String(currTime.getHours()).length == 1) {
+    currHour = "0" + String(currTime.getHours());
+  } else {
+    currHour = String(currTime.getHours());
+  }
+
+  document.getElementById("header").innerHTML =
+    days[currTime.getDay()] +
+    " " +
+    months[currTime.getMonth()] +
+    " " +
+    currTime.getDate() +
+    " " +
+    currHour +
+    ":" +
+    currMin;
 }
 
 var secondCheck = setInterval(tick, 1000);
